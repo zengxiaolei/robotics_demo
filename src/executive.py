8f906fa958a9f19d8ceca5565379cb75b6084c09
Lei Zeng <519464161@qq.com>
2019-08-22 13:32:23 +0800

    Make The Second Turtle Stop When the First Gets Too Close

--- src/executive.py
+++ src/executive.py
@@ -3,11 +3,16 @@
 from turtlesim.srv import Spawn, TeleportAbsolute, TeleportAbsoluteRequest
 from turtle_actionlib.msg import ShapeAction, ShapeActionGoal
 from std_srvs.srv import Empty
+import std_msgs.msg
 import smach
 import smach_ros
 import rospy
 import roslib
 roslib.load_manifest('smach_usecase')
+
+
+def monitor_cb(ud, msg):
+    return False
 
 
 def main():
@@ -41,11 +46,24 @@ def main():
             smach.Concurrence.add('BIG', smach_ros.SimpleActionState(
                 'turtle_shape1', ShapeAction, goal=shape_goal1.goal))
 
-            shape_goal2 = ShapeActionGoal()
-            shape_goal2.goal.edges = 6
-            shape_goal2.goal.radius = 1.0
-            smach.Concurrence.add('SMALL', smach_ros.SimpleActionState(
-                'turtle_shape2', ShapeAction, goal=shape_goal2.goal))
+            sm_con_monitor = smach.Concurrence(outcomes=['succeeded', 'aborted', 'preempted'],
+                                               default_outcome='succeeded',
+                                               child_termination_cb=lambda state_outcomes: True,
+                                               outcome_map={'succeeded': {'DRAW': 'succeeded'},
+                                                            'aborted': {'MONITOR': 'invalid'},
+                                                            'preempted': {'MONITOR': 'preempted', 'DRAW': 'preempted'}})
+            with sm_con_monitor:
+                shape_goal2 = ShapeActionGoal()
+                shape_goal2.goal.edges = 6
+                shape_goal2.goal.radius = 0.5
+                smach.Concurrence.add('DRAW', smach_ros.SimpleActionState(
+                    'turtle_shape2', ShapeAction, goal=shape_goal2.goal))
+
+                smach.Concurrence.add('MONITOR', smach_ros.MonitorState(
+                    "/turtle2_stop", std_msgs.msg.Empty, monitor_cb))
+
+            smach.Concurrence.add('SMALL', sm_con_monitor)
+
         smach.StateMachine.add('DRAW_SHAPES', sm_concurrence)
 
         # shape_goal1 = ShapeActionGoal()
